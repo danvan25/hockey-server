797d2835699e8a0d5025010e4e6ac7f2b1914608
danvan25 <magyardaniel2001@gmail.com>
2026-08-01 21:42:39 +0200

    Initial Spring Boot server with health endpoint

---
+++ src/main/java/com/example/hockeyserver/HockeyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hockeyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HockeyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hockeyserver/HockeyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hockeyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
